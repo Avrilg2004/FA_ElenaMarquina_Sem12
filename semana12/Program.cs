4f102d3babd59d6656f86babf07c3e1b0c1dc0a1
avi <avi@gmail.com>
2025-11-08 14:27:45 -0500

    holi

---
+++ semana12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Holi");
+
+            Console.WriteLine("\nMis apellidos y nombres son:");
+            Console.WriteLine("Marquina Huachaca, Elena Avril Geraldine");
+        }
+    }
+}
